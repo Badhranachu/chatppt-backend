1b280815c21a52b950a9e93e83a05e252d508f7c
Achu . <www.badhranks678@gmail.com>
2025-11-29 15:32:35 +0530

    commit 3:32

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from api.views import ChatPPTView
+from api.views import chat
 
 urlpatterns = [
-    path("chat/", ChatPPTView.as_view(), name="chatppt-chat"),
+    path("chat/", chat.as_view(), name="chatppt-chat"),
 ]
